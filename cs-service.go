08816d96c092a4df75cec7d2a6caf3845ff7105b
Oscar Villavicencio <odvcencio@gmail.com>
2018-11-03 18:56:32 -0400

    still debugging auth for this guy but not main app service

--- cs-service.go
+++ cs-service.go
@@ -2,19 +2,8 @@ package main
 
 import (
 	"csportal-server/server"
-	"fmt"
-	"log"
-	"os"
 )
 
 func main() {
-	region := os.Getenv("AWS_REGION")
-	userPoolID := os.Getenv("COGNITO_USER_POOL_ID")
-
-	jwkURL := fmt.Sprintf("https://cognito-idp.%v.amazonaws.com/%v/.well-known/jwks.json", region, userPoolID)
-	jwk := server.GetJWK(jwkURL)
-
-	log.Println(jwk)
-
 	server.CreateAndListen()
 }
