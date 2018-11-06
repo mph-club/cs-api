1c2ad0cb4536102b65d085ed10cec6fa81732f38
Oscar Villavicencio <odvcencio@gmail.com>
2018-11-06 17:05:54 -0500

    deleting vendor folder and adding gitignore

--- server/server.go
+++ server/server.go
@@ -1,12 +1,20 @@
 package server
 
-import "github.com/kataras/iris"
+import (
+	"github.com/iris-contrib/middleware/cors"
+	"github.com/kataras/iris"
+)
 
 // CreateAndListen exposes the listen and creation of the api
 func CreateAndListen() {
 	_api := iris.New()
 
-	v1 := _api.Party("api/v1", cognitoAuth)
+	crs := cors.New(cors.Options{
+		AllowedOrigins:   []string{"*"}, // allows everything, use that to change the hosts.
+		AllowCredentials: true,
+	})
+
+	v1 := _api.Party("api/v1", cognitoAuth, crs).AllowMethods(iris.MethodOptions)
 	{
 		v1.Get("/home", func(ctx iris.Context) {
 			ctx.Writef("cs portal home!!!!")
